b5ef9768b31fd8ecd23686451bfaae203bec590a
dimysenpai <dimitrymabom@gmail.com>
2023-04-18 13:56:43 +0100

    config router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import './App.css'
+import Router from "./Router";
 
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+  return <Router />;
 }
