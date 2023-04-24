3ffeed63bdc85f14634dfefe3ba04cc04ec4bb3f
dimysenpai <dimitrymabom@gmail.com>
2023-04-24 15:53:45 +0100

    config tailwind work

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import Logo from "../assets/images/GIC_BONUS_LOGO.png";
-import LogoT from "../assets/images/GIC_BONUS_LOGO.png";
+import LogoT from "../assets/images/GIC_BONUS_TEXT.png";
 
 function Header() {
   return (
-    <div>
+    <div className="p-10 bg-black">
       <div>
         <img src={Logo} alt="Logo" />
+        <img src={LogoT} alt="Logo" />
       </div>
-      <div>Nav</div>
-      <div>login</div>
+      <div>
+        <ul>
+          <a href="">
+            <li>Accueil</li>
+          </a>
+          <a href="">
+            <li>Services</li>
+          </a>
+          <a href="">
+            <li>Formations</li>
+          </a>
+          <a href="">
+            <li>A Propos</li>
+          </a>
+        </ul>
+      </div>
+      <div>
+        <button>Inscription</button>
+        <button>Connexion</button>
+      </div>
     </div>
   );
 }
